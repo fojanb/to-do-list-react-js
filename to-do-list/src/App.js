5f3eb8f735df5736fd1a1d40f135e26e65ab5cd9
Fojan Babaali <fozhanmail@gmail.com>
2021-02-27 21:59:03 -0800

    Update App.js

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,19 +1,35 @@
 // import './App.css';
-import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import InputComponent from "./components/InputComponent";
 
 class App extends Component {
-  state = {  }
-  render() { 
-    return ( 
+  state = {
+    items: [],
+  };
+
+  // -----------EventHandlers <Start>---------------
+  getItemHandler = (event) => {
+    let items = event.target.value;
+    this.setState({ items: items });
+  };
+
+  
+  // -----------EventHandlers <Finish>---------------
+
+  render() {
+    
+    return (
       <div>
         <h2>MY LIST</h2>
         <p>Add an Item...</p>
-      
+        <InputComponent
+          getItem={this.getItemHandler}
+          addItem={this.addItemHandler}
+        />
       </div>
     );
   }
 }
- 
-export default App;
 
+export default App;
