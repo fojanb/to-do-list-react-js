3f02e668dbcc46e32d1bd61cc74ab10c8521c0d4
Fojan Babaali <fozhanmail@gmail.com>
2021-02-27 20:47:58 -0800

    Update inputComponent.js

---
+++ to-do-list/src/components/inputComponent.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class InputComponent extends Component {
+    state = {  }
+    render() { 
+        return (  );
+    }
+}
+ 
+export default InputComponent;
