1c43b82a0945573e7e34d3bf5e7b1fefe726730d
Fojan Babaali <fozhanmail@gmail.com>
2021-03-01 23:14:55 -0800

    Update App.js

--- to-do-list/src/App.js
+++ to-do-list/src/App.js
@@ -1,32 +1,42 @@
 // import './App.css';
 import React, { Component } from "react";
-import ReactDOM from "react-dom";
+// import ReactDOM from "react-dom";
 import InputComponent from "./components/InputComponent";
 
 class App extends Component {
   state = {
     items: [],
+    showItems: false,
   };
-
+  styles = {
+    display: "flex",
+    flexDirection: "row",
+  };
   // -----------EventHandlers <Start>---------------
   getItemHandler = (event) => {
     let items = event.target.value;
     this.setState({ items: items });
   };
+  addItemHandler = () => {
+    let paragraph = document.createElement("p");
+    paragraph.innerHTML = this.state.items;
+    document.body.appendChild(paragraph);
+  };
 
-  
   // -----------EventHandlers <Finish>---------------
-
   render() {
-    
     return (
       <div>
         <h2>MY LIST</h2>
         <p>Add an Item...</p>
         <InputComponent
           getItem={this.getItemHandler}
-          addItem={this.addItemHandler}
+          itemEntered={this.state.items}
         />
+        <button type="submit" onClick={this.addItemHandler}>
+          Add
+        </button>
+       
       </div>
     );
   }
