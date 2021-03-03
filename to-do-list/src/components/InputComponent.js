fd545fed9f6cafb538575cbeebaf7541cde2ca45
Fojan Babaali <fozhanmail@gmail.com>
2021-03-02 21:31:23 -0800

    Update InputComponent.js

--- to-do-list/src/components/InputComponent.js
+++ to-do-list/src/components/InputComponent.js
@@ -1,13 +1,13 @@
 import React, { Component } from "react";
 import "./InputComponent.css";
-
 // *********************************************
 class InputComponent extends Component {
   state = this.props.itemEntered;
   render() {
     return (
       <div id="InputComponentDiv">
-        <input id="InputComponentFieldItem"
+        <input
+          id="InputComponentFieldItem"
           type="text"
           placeholder="Type Item Here..."
           onChange={this.props.getItem}
