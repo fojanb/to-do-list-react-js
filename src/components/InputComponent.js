89cd21414d82b167e7efbe161c408d0fae1bf52b
Fojan Babaali <fozhanmail@gmail.com>
2021-03-04 20:28:52 -0800

    Create InputComponent.js

---
+++ src/components/InputComponent.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./InputComponent.css";
+// *********************************************
+class InputComponent extends Component {
+  state = this.props.itemEntered;
+  render() {
+    return (
+      <div id="InputComponentDiv">
+        <input
+          id="InputComponentFieldItem"
+          type="text"
+          placeholder="✎ Write Here "
+          onChange={this.props.getItem}
+        ></input>
+      </div>
+    );
+  }
+}
+
+export default InputComponent;
