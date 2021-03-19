7f986f82d256cf3bf12062b4f72f8be462a852c8
Fojan Babaali <fozhanmail@gmail.com>
2021-03-19 13:24:30 -0700

    Update main.js

--- Vanilla JS version/main.js
+++ Vanilla JS version/main.js
@@ -1,8 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
-//   const tasks = document.querySelector("#tasks");
+  //   const tasks = document.querySelector("#tasks");
   document.getElementById("task").addEventListener("change", addTask);
   function addTask() {
     let task = document.getElementById("task").value;
-    document.getElementById("tasks").innerHTML = task;
+
+    let holder = document.createElement("div");
+    holder.classList.add("tasks");
+    // so far : <div class="tasks"></div>
+    document.getElementById('wrapper').appendChild(holder).innerHTML = task;
+
   }
 });
