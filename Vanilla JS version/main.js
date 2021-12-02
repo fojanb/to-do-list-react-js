d99d47a9091e0aa436a3a0098d59896a74d2c74a
Fojan Babaali <fozhanmail@gmail.com>
2021-12-01 22:41:18 -0800

    Refactoring JS file

--- Vanilla JS version/main.js
+++ Vanilla JS version/main.js
@@ -1,22 +1,20 @@
-document.getElementById("task").addEventListener("change", addTask);
-function addTask() {
-  //from input field
-  let task = document.getElementById("task").value;
-  // ------------------------------------*
-  let holder = document.createElement("div");
+const wrapper = document.querySelector(".wrapper");
+const form = document.querySelector("#form");
+const task = document.querySelector("#task");
+task.addEventListener("change", () => {
+  let newTask = task.value;
+  const holder = document.createElement("div");
   holder.classList.add("task");
-  // So far : <div class="tasks"></div>
-  // ------------------------------------*
-  document.getElementById("wrapper").appendChild(holder).innerHTML =
-    "♡ " + task;
-  // ------------------------------------*
-  let removeButton = document.createElement("button");
+  wrapper.appendChild(holder).innerHTML = newTask;
+  const removeButton = document.createElement("button");
   removeButton.classList.add("removeButtonStyle");
   removeButton.innerText = "DELETE";
-  // So far : <button class="removeButtonStyle">-</button>
   holder.appendChild(removeButton);
-  removeButton.addEventListener("click", removeItem);
-  function removeItem() {
+  removeButton.addEventListener("click", () => {
     holder.style.display = "none";
-  }
-}
+  });
+});
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  task.value = "";
+});
