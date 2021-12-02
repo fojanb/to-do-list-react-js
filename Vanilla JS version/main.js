afab4b4f15aac725d1c349443c4329359cf5bd63
Fojan Babaali <fozhanmail@gmail.com>
2021-12-01 21:40:37 -0800

    Colors updated

--- Vanilla JS version/main.js
+++ Vanilla JS version/main.js
@@ -1,24 +1,22 @@
-document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById("task").addEventListener("change", addTask);
-  function addTask() {
-    //from input field
-    let task = document.getElementById("task").value;
-    // ------------------------------------*
-    let holder = document.createElement("div");
-    holder.classList.add("task");
-    // So far : <div class="tasks"></div>
-    // ------------------------------------*
-    document.getElementById("wrapper").appendChild(holder).innerHTML =
-      "♡ " + task;
-    // ------------------------------------*
-    let removeButton = document.createElement("button");
-    removeButton.classList.add("removeButtonStyle");
-    removeButton.innerText = "-";
-    // So far : <button class="removeButtonStyle">-</button>
-    holder.appendChild(removeButton);
-    removeButton.addEventListener("click", removeItem);
-    function removeItem() {
-      holder.style.display = "none";
-    }
+document.getElementById("task").addEventListener("change", addTask);
+function addTask() {
+  //from input field
+  let task = document.getElementById("task").value;
+  // ------------------------------------*
+  let holder = document.createElement("div");
+  holder.classList.add("task");
+  // So far : <div class="tasks"></div>
+  // ------------------------------------*
+  document.getElementById("wrapper").appendChild(holder).innerHTML =
+    "♡ " + task;
+  // ------------------------------------*
+  let removeButton = document.createElement("button");
+  removeButton.classList.add("removeButtonStyle");
+  removeButton.innerText = "-";
+  // So far : <button class="removeButtonStyle">-</button>
+  holder.appendChild(removeButton);
+  removeButton.addEventListener("click", removeItem);
+  function removeItem() {
+    holder.style.display = "none";
   }
-});
+}
