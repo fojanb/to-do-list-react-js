1321d6d3c88462f4e933287cba0a633e59717cd8
Fojan Babaali <fozhanmail@gmail.com>
2021-12-20 18:04:07 -0800

    Formatting data

--- src/components/Task/Task.js
+++ src/components/Task/Task.js
@@ -3,17 +3,20 @@ const Task = (props) => {
   let { task } = props;
   // console.log("Stringified State:",task);
   const taskList = task.map((item) => JSON.parse(item));
-  // console.log("Parsed State:",taskList)
+  // console.log("Parsed State:",taskList);
   return (
     <div className="flex items-left">
       <ul>
         {taskList !== undefined
           ? taskList.map((item, index) => (
               <li
-                className="flex flex-row border border-greeny rounded-lg"
+                className="flex flex-row justtify-center items-center border border-greeny rounded-lg w-60 mb-12 p-8"
                 key={index}
               >
-                {item.title}
+                <div className="flex flex-row justify-center items-center">
+                  <input type="checkbox" className="m-0 m-auto"/>
+                  <span className="pl-1 m-0 m-auto">{item.title}</span>
+                </div>
               </li>
             ))
           : null}
